273f1803b333f4cb28f8323e8d302530e8efbb76
Viktoriia Popel <xructa2009@gmail.com>
2025-10-04 00:28:32 +0200

    fix changes

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 // src/main.tsx
 
-import React from "react";
+
 import ReactDOM from "react-dom/client";
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
